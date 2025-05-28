e94f6dc8c2c36cc39d4640e7ad5330fbdc485065
Samuel0072 <samlitepu0072@gmail.com>
2025-05-28 20:19:35 +0200

    Update site content

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 export const LoadingScreen = ({ onComplete }) => {
   const [text, setText] = useState("");
-  const fullText = "<TmVne.... />";
+  const fullText = "<Samuel lutepo portifolio.... />";
 
   useEffect(() => {
     let index = 0;
